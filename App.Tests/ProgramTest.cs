49264e1f0756d8e2f3474a9fc79263c0ccaa3511
Frederik Raisa <frederikblunck@gmail.com>
2022-09-03 00:32:24 +0200

    Create test for userprompt and implement user interface.

--- App.Tests/ProgramTest.cs
+++ App.Tests/ProgramTest.cs
@@ -1,5 +1,5 @@
 namespace App.Tests;
-public class UnitTest1
+public class ProgramTest
 {
     [Fact]
     public void is_divisible_by_four()
@@ -23,5 +23,22 @@ public class UnitTest1
         var app = new App.Program();
 
         Assert.True(app.IsLeapYear(1600), "Year should be divisible by 4 unless by 100 unless by 400");
+    }
+
+
+    [Fact]
+    public void will_return_yay_on_leap_year()
+    {
+        var app = new App.Program();
+
+        Assert.Equal(app.PromptForLeapYear(1600), "yay");
+    }
+
+    [Fact]
+    public void will_return_nay_on_any_other_year()
+    {
+        var app = new App.Program();
+
+        Assert.Equal(app.PromptForLeapYear(1234), "nay");
     }
 }
--- App/Program.cs
+++ App/Program.cs
@@ -6,6 +6,22 @@ namespace App
     {
         public bool IsLeapYear(int year) => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 
-        public static void Main() { }
+        public String PromptForLeapYear(int year)
+        {
+            return IsLeapYear(year) switch
+            {
+                true => "yay",
+                false => "nay"
+            };
+        }
+
+        public static void Main()
+        {
+            var app = new Program();
+
+            Console.Write("Check if leap year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(app.PromptForLeapYear(year));
+        }
     }
 }
