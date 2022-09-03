f11b85b2aec3ac4b62eb751efddc9a6747815800
Frederik <mfoman@users.noreply.github.com>
2022-09-03 12:10:30 +0200

    Develop (#2)
    * Add solution files and projects
    
    * Test and implement isLeapYear
    
    * Create test for userprompt and implement user interface.
    
    * Implement error handling
    
    * Document functions
    
    * Add diagrams for the leapyear function
    
    * Merge main and fix test

---
+++ App.Tests/ProgramTest.cs
@@ -0,0 +1,37 @@
+namespace App.Tests;
+
+public class ProgramTest
+{
+    [Fact]
+    public void is_divisible_by_four()
+    {
+        var app = new App.Program();
+
+        Assert.True(app.IsLeapYear(4), "Year should be divisible by 4");
+    }
+
+    [Fact]
+    public void is_divisible_by_four_unless_also_by_100()
+    {
+        var app = new App.Program();
+
+        Assert.False(app.IsLeapYear(200), "Year should be divisible by 4");
+    }
+
+    [Fact]
+    public void is_divisible_by_four_unless_also_by_100_unless_divisible_by_400()
+    {
+        var app = new App.Program();
+
+        Assert.True(app.IsLeapYear(1600), "Year should be divisible by 4 unless by 100 unless by 400");
+    }
+
+    [Fact]
+    public void returns_yay_or_nay()
+    {
+        var app = new App.Program();
+
+        Assert.Equal("yay", app.GetResponse(true));
+        Assert.Equal("nay", app.GetResponse(false));
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace App
+{
+    public class Program
+    {
+        /// <summary>
+        /// A method to check if given year is a leap year
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns>true if year is true and false if not</returns>
+        public bool IsLeapYear(int year) => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+
+        /// <summary>
+        /// Prompts the user and returns the response
+        /// </summary>
+        /// <returns>Returns the string given from GetResponse</returns>
+        public String PromptForLeapYear()
+        {
+            Console.Write("Check if is leap year: ");
+
+            int year = GetInputAsIntOrThrowFormatException();
+
+            bool isLeapYear = IsLeapYear(year);
+
+            return GetResponse(isLeapYear);
+        }
+
+        /// <summary>
+        /// Returns a literal string response depending on the input
+        /// </summary>
+        /// <param name="isLeapYear"></param>
+        /// <returns>
+        /// Returns yay for true and nay for false
+        /// </returns>
+        public String GetResponse(bool isLeapYear)
+        {
+            return isLeapYear ? "yay" : "nay";
+        }
+
+        /// <summary>
+        /// Gets input from the console and casts it to an int, else throws an exception.
+        /// </summary>
+        /// <returns>The year given from input</returns>
+        /// <exception cref="FormatException"></exception>
+        public int GetInputAsIntOrThrowFormatException()
+        {
+            try
+            {
+                return new ConsoleRetriever().GetInt();
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Sorry, only numbers are allowed!");
+            }
+        }
+
+        public void Run()
+        {
+            string response = PromptForLeapYear();
+            Console.WriteLine(response);
+        }
+
+        public static void Main()
+        {
+            try
+            {
+                new Program().Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
