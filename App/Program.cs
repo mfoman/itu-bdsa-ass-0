2b8aa6da31b1a01e3119cb1cf1cd6378222a90fd
Frederik Raisa <frederikblunck@gmail.com>
2022-09-03 11:16:14 +0200

    Document functions

--- App/Program.cs
+++ App/Program.cs
@@ -4,8 +4,17 @@ namespace App
 {
     public class Program
     {
+        /// <summary>
+        /// A method to check if given year is a leap year
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns>true if year is true and false if not</returns>
         public bool IsLeapYear(int year) => year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 
+        /// <summary>
+        /// Prompts the user and returns the response
+        /// </summary>
+        /// <returns>Returns the string given from GetResponse</returns>
         public String PromptForLeapYear()
         {
             Console.Write("Check if is leap year: ");
@@ -17,11 +26,23 @@ namespace App
             return GetResponse(isLeapYear);
         }
 
+        /// <summary>
+        /// Returns a literal string response depending on the input
+        /// </summary>
+        /// <param name="isLeapYear"></param>
+        /// <returns>
+        /// Returns yay for true and nay for false
+        /// </returns>
         public String GetResponse(bool isLeapYear)
         {
             return isLeapYear ? "yay" : "nay";
         }
 
+        /// <summary>
+        /// Gets input from the console and casts it to an int, else throws an exception.
+        /// </summary>
+        /// <returns>The year given from input</returns>
+        /// <exception cref="FormatException"></exception>
         public int GetInputAsIntOrThrowFormatException()
         {
             try
